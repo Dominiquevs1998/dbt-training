072c07866a46b23efda52364399fbcf17f757da0
Dominique van Schagen <dominique@rbi-solutions.nl>
2024-03-05 13:03:57 +0000

    added delivery team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,6 @@
-SELECT o.orderid
+SELECT 
+    {{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} AS sk_orders
+    ,o.orderid
     ,o.orderdate
     ,o.shipdate
     ,o.shipmode
@@ -14,9 +16,12 @@ SELECT o.orderid
     ,p.productname
     ,p.subcategory
     ,{{ markup('ordersellingprice', 'ordercostprice') }} AS markup
+    ,d.delivery_team
 FROM {{ ref('raw_orders') }} o 
 LEFT JOIN {{ ref('raw_customer') }} c
     ON o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_product') }} p
     ON o.productid = p.productid
+LEFT JOIN {{ ref('delivery_team') }} d
+    ON o.shipmode = d.shipmode
 {{ limit_data_in_dev(orderdate) }}
